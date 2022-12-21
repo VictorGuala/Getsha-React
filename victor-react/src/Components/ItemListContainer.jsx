494b231216e1e4c0605919b72818a732c455baa0
Victor Guala <victorguala@gmail.com>
2022-12-21 12:23:40 -0300

    firebase

--- victor-react/src/Components/ItemListContainer.jsx
+++ victor-react/src/Components/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import arrayProducts from "./Json/Products.json";
-import ItemCount from "./ItemCount";
+// import ItemCount from "./ItemCount";
 import ItemList from "./ItemList";
 
 const ItemListContainer = () => {
--- victor-react/src/index.js
+++ victor-react/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCss6l00zWdFYYFpYlX3jIScv6J75o7wJc",
+  authDomain: "espacio-getsha.firebaseapp.com",
+  projectId: "espacio-getsha",
+  storageBucket: "espacio-getsha.appspot.com",
+  messagingSenderId: "257664318882",
+  appId: "1:257664318882:web:0f37767ece2dc0d10c46fb"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />
 );
